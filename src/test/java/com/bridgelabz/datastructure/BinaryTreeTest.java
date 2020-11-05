3d47921abdf0e52fe96d0da8b9eb4f735144edb4
Sampada-VS <samssv1@gmail.com>
2020-11-06 03:46:33 +0530

    Adding-complete binary tree with 13 nodes

--- src/test/java/com/bridgelabz/datastructure/BinaryTreeTest.java
+++ src/test/java/com/bridgelabz/datastructure/BinaryTreeTest.java
@@ -12,8 +12,29 @@ public class BinaryTreeTest {
 		binaryTree.add(56);
 		binaryTree.add(30);
 		binaryTree.add(70);
+		
 		int size =binaryTree.getSize();
 		assertEquals(3,size);
+	}
+	@Test
+	public void GivenThirteenNodes_WhenAddedToTree_ShouldReturnSize13() {
+		BinaryTree<Integer> binaryTree=new BinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(11);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(65);
+		binaryTree.add(63);
+		binaryTree.add(67);
+		
+		int size =binaryTree.getSize();
+		assertEquals(13,size);
 	}
 
 }
