a6580f8e5331bd057c4d6049fd05839966dbbe97
Sampada-VS <samssv1@gmail.com>
2020-11-06 03:10:12 +0530

    Adding-creating binary tree and returned size

---
+++ src/main/java/com/bridgelabz/datastructure/BinaryTree.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.datastructure;
+
+public class BinaryTree<K extends Comparable<K>>{
+	private BinaryNode<K> root;
+	public void add (K key) {
+		this.root=this.addRecursively(root,key);
+	}
+	private BinaryNode<K> addRecursively(BinaryNode<K>current, K key){
+		if (current == null) 
+			return new BinaryNode<>(key);
+		int compareResult=key.compareTo(current.key);
+		if (compareResult== 0)
+			return current;
+		if(compareResult < 0)
+			current.left=addRecursively(current.left, key);
+		else
+			current.right=addRecursively(current.right, key);
+		return current;
+	}
+	public int getSize() {
+		return this.getSizeRecursively(root);
+	}
+	private int getSizeRecursively(BinaryNode<K> current) {
+		return current == null ? 0 : 1 + this.getSizeRecursively(current.left)
+										+this.getSizeRecursively(current.right);
+	}
+}
---
+++ src/test/java/com/bridgelabz/datastructure/BinaryTreeTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.datastructure;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinaryTreeTest {
+
+	@Test
+	public void GivenThreeNodes_WhenAddedToTree_ShouldReturnSize3() {
+		BinaryTree<Integer> binaryTree=new BinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		int size=binaryTree.getSize();
+		assertEquals(3,size);
+	}
+
+}
